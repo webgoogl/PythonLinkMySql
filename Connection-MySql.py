1d417bfb99b4a4d35c7a1d616fa78ca5356e81c8
Suraj upadhyay <136471722+webgoogl@users.noreply.github.com>
2024-11-28 00:40:10 -0800

    Add files via upload

---
+++ Connection-MySql.py
@@ -0,0 +1,16 @@
+import mysql.connector as sq
+
+'''con=sq.connect( user = " username ",
+                passwd =" password ",
+                host = " host ",
+                database = " database "
+    )
+
+cursor=con.cursor()
+
+query="Select * from table "
+cursor.execute(query)
+
+cursor.close()
+con.close()'''
+
